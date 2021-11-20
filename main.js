3778658ea17d9b8aa780d7fc2f0324eb72c69bf8
Whitepotter <89248447+Whitepotter@users.noreply.github.com>
2021-11-20 15:21:33 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,47 @@
+var mouseevent = "empty";
+var last_position_of_x,last_position_of_y;
+
+canvas = document.getElementById("myCanvas");
+ctx = canvas.getContext("2d");
+colour = "black";
+width_of_line = 1;
+
+canvas.addEventListener("mousedown",my_mousedown);
+function my_mousedown(e){
+
+    colour = document.getElementById("Colour").value;
+    width_of_line = document.getElementById("width").value;
+    radius = document.getElementById("radius").value;
+    mouseevent = "mouseDown";
+}
+
+canvas.addEventListener("mousemove",my_mousemove);
+function my_mousemove(e){
+
+    current_position_of_mouse_x = e.clientX - canvas.offsetLeft;
+    current_position_of_mouse_y = e.clientY - canvas.offsetTop;
+
+    if (mouseevent == "mouseDown"){
+        ctx.beginPath();
+        ctx.lineWidth = width_of_line;
+        ctx.strokeStyle = colour;
+
+        ctx.arc(current_position_of_mouse_x,current_position_of_mouse_y,radius,0,2*Math.PI);
+        ctx.stroke();
+    }
+    last_position_of_x = current_position_of_mouse_x;
+    last_position_of_y = current_position_of_mouse_y;
+}
+canvas.addEventListener("mouseup",my_mouseup);
+function my_mouseup(e){
+    mouseevent = "mouseUp";
+}
+
+canvas.addEventListener("mouseleave",my_mouseleave);
+function my_mouseleave(e){
+    mouseevent = "mouseLeave";
+}
+
+function cleararea() {
+    ctx.clearRect(0,0,ctx.canvas.width,ctx.canvas.height);
+}
